f830d2eab35176d39d6c1e7792da7aaa27988fe5
Maxwell Iverson <maxwelliverson@gmail.com>
2021-06-10 20:27:10 -0700

    Memory management updates

--- src/quartz/mm.cpp
+++ src/quartz/mm.cpp
@@ -8,6 +8,79 @@
 #include "mm.hpp"
 
 
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+
+#pragma comment(lib, "mincore")
+
+
+
+
+
+void* qtz::virtual_placeholder::reserve(jem_size_t size, jem_size_t alignment) noexcept {
+  if ( alignment == JEM_DONT_CARE ) {
+    return VirtualAlloc2(nullptr, nullptr, size, MEM_RESERVE | MEM_RESERVE_PLACEHOLDER, PAGE_NOACCESS, nullptr, 0);
+  }
+  else {
+    MEM_ADDRESS_REQUIREMENTS addressRequirements{
+      .LowestStartingAddress = nullptr,
+      .HighestEndingAddress  = nullptr,
+      .Alignment = alignment
+    };
+    MEM_EXTENDED_PARAMETER alignParam{
+      .Type    = MemExtendedParameterAddressRequirements,
+      .Pointer = &addressRequirements
+    };
+    return VirtualAlloc2(nullptr, nullptr, size, MEM_RESERVE | MEM_RESERVE_PLACEHOLDER, PAGE_NOACCESS, &alignParam, 1);
+  }
+}
+bool  qtz::virtual_placeholder::release(void* placeholder) noexcept {
+  return VirtualFree(placeholder, 0, MEM_RELEASE);
+}
+
+void* qtz::virtual_placeholder::alloc(void* placeholder, jem_size_t size, memory_access_t access, numa_node_t numaNode) noexcept {
+  if ( numaNode == JEM_CURRENT_NUMA_NODE ) {
+    return VirtualAlloc2(nullptr, placeholder, size, MEM_RESERVE | MEM_COMMIT | MEM_REPLACE_PLACEHOLDER, access, nullptr, 0);
+  }
+  else {
+    MEM_EXTENDED_PARAMETER numaNodeParam{
+      .Type = MemExtendedParameterNumaNode,
+      .ULong = numaNode
+    };
+    return VirtualAlloc2(nullptr, placeholder, size, MEM_RESERVE | MEM_COMMIT | MEM_REPLACE_PLACEHOLDER, access, &numaNodeParam, 1);
+  }
+}
+bool  qtz::virtual_placeholder::free(void* allocation, jem_size_t size) noexcept {
+  return VirtualFree(allocation, size, MEM_RELEASE | MEM_PRESERVE_PLACEHOLDER);
+}
+
+
+void* qtz::virtual_placeholder::map(void* placeholder, const file_mapping& fileMapping, memory_access_t access, numa_node_t numaNode) noexcept {
+  if ( numaNode == JEM_CURRENT_NUMA_NODE ) {
+    return MapViewOfFile3(fileMapping.handle, nullptr, placeholder, 0, fileMapping.size, MEM_REPLACE_PLACEHOLDER, access, nullptr, 0);
+  }
+  else {
+    MEM_EXTENDED_PARAMETER numaNodeParam{
+      .Type = MemExtendedParameterNumaNode,
+      .ULong = numaNode
+    };
+    return MapViewOfFile3(fileMapping.handle, nullptr, placeholder, 0, fileMapping.size, MEM_REPLACE_PLACEHOLDER, access, &numaNodeParam, 1);
+  }
+}
+bool  qtz::virtual_placeholder::unmap(void* placeholder) noexcept {
+  return UnmapViewOfFile2(nullptr, placeholder, MEM_PRESERVE_PLACEHOLDER);
+}
+
+bool qtz::virtual_placeholder::split(void* placeholder, jem_size_t firstSize) noexcept {
+  return VirtualFree();
+}
+bool qtz::virtual_placeholder::coalesce(void* placeholder, jem_size_t totalSize) noexcept {}
+
+
+
+
 JEM_stdcall qtz_request_t   qtz_alloc_static_mailbox(void **pResultAddr, jem_u32_t messageSlots, jem_u32_t messageSize, const qtz_shared_mailbox_params_t *pSharedParams);
 JEM_stdcall void            qtz_free_static_mailbox(void *mailboxAddr);
 JEM_stdcall qtz_request_t   qtz_alloc_dynamic_mailbox(void **pResultAddr, jem_u32_t minMailboxSize, jem_u32_t maxMessageSize, const qtz_shared_mailbox_params_t *pSharedParams);
--- src/quartz/mm.hpp
+++ src/quartz/mm.hpp
@@ -14,10 +14,44 @@
 namespace qtz{
 
 #if JEM_system_windows
-  using native_file_handle_t = void*;
+  using native_handle_t = void*;
 #else
-  using native_file_handle_t = int;
+  using native_handle_t = int;
 #endif
+
+  enum memory_access_t{
+    no_access                = 0x01,
+    readonly_access          = 0x02,
+    readwrite_access         = 0x04,
+    writecopy_access         = 0x08,
+    execute_access           = 0x10,
+    read_execute_access      = 0x20,
+    readwrite_execute_access = 0x40,
+    writecopy_execute_access = 0x80
+  };
+  enum process_permissions_t{
+    can_terminate_process          = 0x00000001,
+    can_create_thread              = 0x00000002,
+    can_operate_on_process_memory  = 0x00000008,
+    can_read_from_process_memory   = 0x00000010,
+    can_write_to_process_memory    = 0x00000020,
+    can_duplicate_handle           = 0x00000040,
+    can_create_process             = 0x00000080,
+    can_set_process_quota          = 0x00000100,
+    can_set_process_info           = 0x00000200,
+    can_query_process_info         = 0x00000400,
+    can_suspend_process            = 0x00000800,
+    can_query_limited_process_info = 0x00001000,
+    can_delete_process             = 0x00010000,
+    can_read_process_security      = 0x00020000,
+    can_modify_process_security    = 0x00040000,
+    can_change_process_owner       = 0x00080000,
+    can_synchronize_with_process   = 0x00100000
+  };
+
+  using numa_node_t = jem_u32_t;
+
+#define JEM_CURRENT_NUMA_NODE ((numa_node_t)-1)
 
   class file{
     inline constexpr static jem_size_t StorageSize  = 2 * sizeof(void*);
@@ -36,8 +70,8 @@ namespace qtz{
 
 
 
-    JEM_nodiscard native_file_handle_t get_native_handle() const noexcept {
-      return *reinterpret_cast<const native_file_handle_t*>(storage + sizeof(void*));
+    JEM_nodiscard native_handle_t get_native_handle() const noexcept {
+      return *reinterpret_cast<const native_handle_t*>(storage + sizeof(void*));
     }
 
   private:
@@ -45,6 +79,7 @@ namespace qtz{
   };
 
   class file_mapping{
+    friend class virtual_placeholder;
   public:
 
   private:
@@ -53,13 +88,441 @@ namespace qtz{
     jem_size_t size;
   };
   struct anonymous_file_mapping{
-    native_file_handle_t handle;
-    jem_size_t           size;
+    native_handle_t handle;
+    jem_size_t      size;
   };
   class mapped_region{};
   class virtual_memory_region{};
 
+  template <typename CharType>
+  class environment;
+
+  template <typename CharType, bool IsConst = false>
+  class environment_variable_ref{
+
+    template <typename>
+    friend class environment;
+
+    using env_ptr = std::conditional_t<IsConst, const environment<CharType>, environment<CharType>>*;
+  public:
+    using char_type = CharType;
+    using environment_type = environment<char_type>;
+    using string_view_type = std::basic_string_view<char_type>;
+
+    environment_variable_ref(env_ptr env, jem_u32_t offset) noexcept
+        : env(env),
+          entryOffset(offset)
+    {}
+
+    environment_variable_ref& operator=(string_view_type newValue) noexcept {
+
+    }
+
+
+    friend bool operator==(environment_variable_ref a, environment_variable_ref b) noexcept {
+      assert(a.env == b.env);
+      return a.entryOffset == b.entryOffset;
+    }
+
+  private:
+    env_ptr   env;
+    jem_u32_t entryOffset;
+  };
+
+  template <typename CharType>
+  class environment{
+
+    template <typename, bool IsConst>
+    friend class environment_variable_ref;
+
+    template <bool IsConst>
+    class iterator_;
+
+    using word_type = jem_u32_t;
+
+    inline constexpr static CharType ZeroChar = static_cast<CharType>(0x00);
+    inline constexpr static CharType EqualsChar = static_cast<CharType>(0x3D);
+#if JEM_system_windows
+    inline constexpr static CharType SeparatorChar = static_cast<CharType>(0x3B);
+#else
+    inline constexpr static CharType SeparatorChar = static_cast<CharType>(0x3A);
+#endif
+
+  public:
+
+    using char_type      = CharType;
+    using string_view    = std::basic_string_view<char_type>;
+    using variable_type  = environment_variable_ref<char_type>;
+    using value_type     = variable_type;
+    using iterator       = iterator_<false>;
+    using const_iterator = iterator_<true>;
+    class sentinel{
+    public:
+      template <bool IsConst>
+      friend bool operator==(const iterator_<IsConst>& iter, sentinel) noexcept {
+        return iter.offset == 0;
+      }
+    };
+    using const_sentinel = sentinel;
+
+
+
+
+
+
+    JEM_nodiscard std::unique_ptr<char_type[]> get_normalized_buffer() const noexcept {
+      auto* output = new char_type[get_required_normalized_size()];
+      jem_u32_t  index = buffer.head->nextOffset;
+      jem_size_t offset = 0;
+      while ( index != 0 ) {
+        const var_t& var = lookup_var(index);
+        const jem_size_t varLength = var.entryStrLen + var.valueStrLen + 1;
+        memcpy(output + offset, var.buffer, varLength);
+        offset += varLength;
+        output[offset++] = ZeroChar;
+        index = var.nextOffset;
+      }
+      output[offset] = ZeroChar;
+      if ( offset == 0 )
+        output[1] = ZeroChar;
+      return { output };
+    }
+
+  private:
+
+    template <bool IsConst>
+    class iterator_{
+      template <bool>
+      friend class iterator_;
+      friend class sentinel;
+      using env_ptr    = std::conditional_t<IsConst, const environment, environment>*;
+    public:
+      using value_type = environment_variable_ref<char_type, IsConst>;
+      using reference  = std::conditional_t<IsConst, const value_type, value_type>&;
+
+      explicit iterator_(env_ptr env, jem_u32_t index) noexcept
+          : env(env),
+            offset(index),
+            mustCache(false),
+            cachedVar(env, index)
+      {}
+      iterator_(const iterator_<false>& other) noexcept
+          : iterator_(other.env, other.offset){}
+
+      reference operator*() const noexcept {
+        if ( mustCache ) {
+          cachedVar.entryOffset = offset;
+          mustCache = false;
+        }
+        return cachedVar;
+      }
+
+      iterator_& operator++()    noexcept {
+        offset = env->lookup_var(offset).nextOffset;
+        mustCache = true;
+        return *this;
+      }
+      iterator_  operator++(int) noexcept {
+        iterator_ copy = *this;
+        ++*this;
+        return copy;
+      }
+      iterator_& operator--()    noexcept {
+        offset = env->lookup_var(offset).prevOffset;
+        mustCache = true;
+        return *this;
+      }
+      iterator_  operator--(int) noexcept {
+        iterator_ copy = *this;
+        --*this;
+        return copy;
+      }
+
+
+      friend bool operator==(const iterator_& a, const iterator_& b) noexcept {
+        assert(a.env == b.env);
+        return a.offset == b.offset;
+      }
+
+
+    private:
+      env_ptr            env;
+      jem_u32_t          offset;
+      mutable bool       mustCache = true;
+      mutable value_type cachedVar;
+    };
+
+    struct var_t {
+      jem_u32_t prevOffset;
+      jem_u32_t nextOffset;
+      jem_u16_t entryStrLen;
+      jem_u16_t valueStrLen;
+      char_type buffer[];
+    };
+
+
+
+    void resort_variables() noexcept;
+
+
+    JEM_nodiscard jem_size_t get_required_normalized_size() const noexcept {
+      jem_size_t reqSize = 1;
+      jem_u32_t  index = buffer.head->nextOffset;
+      while ( index != 0 ) {
+        const var_t& var = lookup_var(index);
+        reqSize += (var.entryStrLen + var.valueStrLen + 2);
+        index = var.nextOffset;
+      }
+      if ( reqSize == 1 )
+        return 2;
+      return reqSize;
+    }
+
+
+    JEM_nodiscard bool check_valid_index(jem_u32_t index) const noexcept {
+      return index < (bufferLength / alignof(word_type));
+    }
+    var_t&             lookup_var(jem_u32_t index) noexcept {
+      return reinterpret_cast<var_t&>(buffer.words[index]);
+    }
+    const var_t&       lookup_var(jem_u32_t index) const noexcept {
+      return reinterpret_cast<const var_t&>(buffer.words[index]);
+    }
+
+    void remove_variable(jem_u32_t index) noexcept {
+      remove_variable(lookup_var(index));
+    }
+    void remove_variable(var_t& var) noexcept {
+      lookup_var(var.prevOffset).nextOffset = var.nextOffset;
+      lookup_var(var.nextOffset).prevOffset = var.prevOffset;
+    }
+    void insert_variable_after(jem_u32_t position, jem_u32_t index) noexcept {
+      var_t& var = lookup_var(index);
+      var_t& prev  = lookup_var(position);
+      var.prevOffset = position;
+      var.nextOffset = prev.nextOffset;
+      lookup_var(prev.nextOffset).prevOffset = index;
+      prev.nextOffset  = index;
+    }
+    void insert_variable_before(jem_u32_t position, jem_u32_t index) noexcept {
+      var_t& var = lookup_var(index);
+      var_t& next  = lookup_var(position);
+      var.nextOffset = position;
+      var.prevOffset = next.prevOffset;
+      lookup_var(next.prevOffset).nextOffset = index;
+      next.prevOffset  = index;
+    }
+
+
+    void swap_out_variable(jem_u32_t oldIndex, jem_u32_t newIndex) noexcept {
+      var_t& oldVar = lookup_var(oldIndex);
+      var_t& newVar = lookup_var(newIndex);
+      newVar.prevOffset = oldVar.prevOffset;
+      newVar.nextOffset = oldVar.nextOffset;
+      lookup_var(oldVar.prevOffset).nextOffset = newIndex;
+      lookup_var(oldVar.nextOffset).prevOffset = newIndex;
+    }
+
+
+    void reserve(jem_size_t size) noexcept {
+      if ( size <= bufferCapacity )
+        return;
+      bufferCapacity = ((size - 1) | (alignof(word_type) - 1)) + 1;
+      buffer.words = static_cast<word_type*>(realloc(buffer.words, bufferCapacity));
+    }
+    void grow_by(jem_size_t size) noexcept {
+      bufferLength += size;
+      if ( bufferCapacity < bufferLength ) {
+        reserve(bufferLength * 3 / 2);
+      }
+    }
+
+    JEM_nodiscard JEM_forceinline static jem_size_t required_growth_size(jem_u32_t entryLength, jem_u32_t valueLength) noexcept {
+      constexpr static jem_size_t AlignMask = alignof(word_type) - 1;
+      const jem_size_t charsReq = entryLength + valueLength;
+      return sizeof(var_t) + (charsReq | AlignMask) + 1;
+    }
+
+    JEM_nodiscard JEM_forceinline static char_type* get_entry(const var_t& var) noexcept {
+      return var.buffer;
+    }
+    JEM_nodiscard JEM_forceinline static char_type* get_value(const var_t& var) noexcept {
+      return var.buffer + var.entryStrLen + 1;
+    }
+
+    jem_u32_t allocate_new_variable(jem_u32_t entryLength, jem_u32_t valueLength) noexcept {
+      const jem_u32_t index = bufferLength / alignof(word_type);
+      grow_by(required_growth_size(entryLength, valueLength));
+      return index;
+    }
+
+
+    jem_u32_t modify_variable_value(jem_u32_t index, string_view newValue) noexcept {
+      var_t& var = lookup_var(index);
+      if ( newValue.length() <= var.valueStrLen ) {
+        memmove(get_value(var), newValue.data(), newValue.size() * sizeof(char_type));
+        var.valueStrLen = jem_u16_t(newValue.size());
+        return index;
+      }
+      const jem_u32_t newIndex = allocate_new_variable(var.entryStrLen, newValue.size());
+      var_t& newVar = lookup_var(newIndex);
+      newVar.valueStrLen = jem_u16_t(newValue.size());
+      newVar.entryStrLen = var.entryStrLen;
+      memcpy(get_entry(newVar), get_entry(var), (var.entryStrLen + 1) * sizeof(char_type));
+      memcpy(get_value(newVar), newValue.data(), newValue.size() * sizeof(char_type));
+      swap_out_variable(index, newIndex);
+      return newIndex;
+    }
+    jem_u32_t modify_variable_entry(jem_u32_t index, string_view newEntry) noexcept {
+      var_t& var = lookup_var(index);
+      char_type* const oldValueStr = get_value(var);
+
+      if ( newEntry.length() <= var.entryStrLen ) {
+        memmove(get_entry(var), newEntry.data(), newEntry.size() * sizeof(char_type));
+        var.buffer[newEntry.size()] = EqualsChar;
+        var.entryStrLen = jem_u16_t(newEntry.size());
+        memmove(get_value(var), oldValueStr, var.valueStrLen);
+        resort_variables();
+        return index;
+      }
+
+      const jem_u32_t newIndex = allocate_new_variable(newEntry.size(), var.valueStrLen);
+      var_t& newVar = lookup_var(newIndex);
+      newVar.entryStrLen = jem_u16_t(newEntry.size());
+      newVar.valueStrLen = var.valueStrLen;
+      memcpy(get_entry(newVar), newEntry.data(), newEntry.size() * sizeof(char_type));
+      newVar.buffer[newEntry.size()] = EqualsChar;
+      memcpy(get_value(newVar), get_value(var), newVar.valueStrLen * sizeof(char_type));
+      swap_out_variable(index, newIndex);
+      resort_variables();
+      return newIndex;
+    }
+    jem_u32_t append_variable_value(jem_u32_t index, string_view newValue) noexcept {
+      var_t& var = lookup_var(index);
+      const jem_u32_t newValueSize = newValue.size() + var.valueStrLen + 1;
+      const jem_u32_t newIndex = allocate_new_variable(var.entryStrLen, newValueSize);
+      var_t& newVar = lookup_var(newIndex);
+      newVar.valueStrLen = jem_u16_t(newValueSize);
+      newVar.entryStrLen = var.entryStrLen;
+      memcpy(get_entry(newVar), get_entry(var), (var.entryStrLen + 1) * sizeof(char_type));
+      memcpy(get_value(newVar), get_value(var), var.valueStrLen * sizeof(char_type));
+      get_value(newVar)[var.valueStrLen] = SeparatorChar;
+      memcpy(get_value(newVar) + var.valueStrLen + 1, newValue.data(), newValue.size() * sizeof(char_type));
+      swap_out_variable(index, newIndex);
+      return newIndex;
+    }
+    jem_u32_t insert_new_variable(jem_u32_t afterIndex, string_view entry, std::span<string_view> values) noexcept {
+      assert( !values.empty() );
+
+      // Sum total required size for the combined value string
+      auto valueStrLen = static_cast<jem_size_t>(-1);
+      for ( string_view val : values )
+        valueStrLen += val.size() + 1;
+      const jem_u32_t newIndex = allocate_new_variable(entry.size(), valueStrLen);
+
+
+
+      var_t& newVar = lookup_var(newIndex);
+      newVar.entryStrLen = entry.size();
+      newVar.valueStrLen = valueStrLen;
+
+      // Copy entry and equals sign
+      memcpy(get_entry(newVar), entry.data(), entry.size() * sizeof(word_type));
+      get_entry(newVar)[entry.size()] = EqualsChar;
+
+      // Copy values and insert separators
+      char_type* valuePos = get_value(newVar);
+      const string_view*       valueBegin = values.begin();
+      const string_view* const valueEnd   = values.end();
+      memcpy(valuePos, valueBegin->data(), valueBegin->size());
+      valuePos += valueBegin->size();
+      while ( ++valueBegin != valueEnd ) {
+        *valuePos++ = SeparatorChar;
+        memcpy(valuePos, valueBegin->data(), valueBegin->size());
+        valuePos += valueBegin->size();
+      }
+
+      insert_variable_after(afterIndex, newIndex);
+      return newIndex;
+    }
+
+
+    jem_u32_t bufferLength;
+    jem_u32_t bufferCapacity;
+    jem_u32_t entryCount;
+    union{
+      word_type* words;
+      var_t*   head;
+    } buffer;
+  };
+
+  class process {
+  public:
+
+    struct id{
+      friend bool operator==(id a, id b) noexcept = default;
+      friend std::strong_ordering operator<=>(id a, id b) noexcept = default;
+    private:
+      friend class process;
+
+      explicit id(jem_u32_t value) noexcept : id_(value){}
+
+      jem_u32_t id_;
+    };
+
+
+    static std::optional<process> create() noexcept;
+    static std::optional<process> open(id processId, process_permissions_t permissions) noexcept;
+
+
+    void* reserve_memory(jem_size_t size, jem_size_t alignment = JEM_DONT_CARE) noexcept;
+    bool  release_memory(void* placeholder) noexcept;
+
+    void* local_alloc(void* address, jem_size_t size, memory_access_t access = readwrite_access, numa_node_t numaNode = JEM_CURRENT_NUMA_NODE) noexcept;
+    bool  local_free(void* allocation, jem_size_t size) noexcept;
+
+    void* map(void* address, const file_mapping& fileMapping, memory_access_t = readwrite_access, numa_node_t numaNode = JEM_CURRENT_NUMA_NODE) noexcept;
+    bool  unmap(void* address) noexcept;
+
+    bool  split(void* placeholder, jem_size_t firstSize) noexcept;
+    bool  coalesce(void* placeholder, jem_size_t totalSize) noexcept;
+
+  private:
+    native_handle_t       handle_;
+    id                    id_;
+    process_permissions_t permissions_;
+  };
+
+
+
+
+
+
+  class virtual_placeholder{
+  public:
+
+
+
+
+
+    static void* reserve(jem_size_t size, jem_size_t alignment = JEM_DONT_CARE) noexcept;
+    static bool  release(void* placeholder) noexcept;
+
+    static void* alloc(void* placeholder, jem_size_t size, memory_access_t access = readwrite_access, numa_node_t numaNode = JEM_CURRENT_NUMA_NODE) noexcept;
+    static bool  free(void* allocation, jem_size_t size) noexcept;
+
+    static void* map(void* placeholder, const file_mapping& fileMapping, memory_access_t = readwrite_access, numa_node_t numaNode = JEM_CURRENT_NUMA_NODE) noexcept;
+    static bool  unmap(void* placeholder) noexcept;
+
+    static bool  split(void* placeholder, jem_size_t firstSize) noexcept;
+    static bool  coalesce(void* placeholder, jem_size_t totalSize) noexcept;
+  };
+
+
+
+
   class extensible_segment {
+    inline constexpr static jem_size_t MagicNumber        = 0xFA81'1B0C'CAC0'182C;
     friend class extensible_segment_ref;
     inline constexpr static jem_size_t MaxChunkNameLength = 256;
   public:
@@ -71,6 +534,7 @@ namespace qtz{
       jem_u64_t              magic;
       anonymous_file_mapping nextMapping;
       anonymous_file_mapping mapping;
+      jem_size_t             chunkSize; // Multiple of alignment
       jem_size_t             alignment;
       jem_size_t             maxCapacity;
       jem_size_t             currentCapacity;
@@ -84,7 +548,6 @@ namespace qtz{
     };
 
     header_t*  header;    // Address is aligned to alignment
-    jem_size_t chunkSize; // Multiple of alignment
   };
 
   class extensible_segment_ref {
